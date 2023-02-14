b8cd0f28142e88d28b8582fce6b5db6c84f8b4af
alibabaeefml <alisource79@gmail.com>
2023-02-14 12:13:23 +0330

    commit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,14 +8,31 @@ $(function () {
   } catch (e) {
     console.error(e);
   }
+
   $('[href=""]').attr("href", "javascript:void(0)");
+
+  // set masks
   $("[mask-url]").each((i, e) => {
     // inner
-    e.setAttribute('style',`-webkit-mask-box-image:${$(e).attr("mask-url")};
+    e.setAttribute(
+      "style",
+      `-webkit-mask-box-image:${$(e).attr("mask-url")};
     mask-image:${$(e).attr("mask-url")};
-    -webkit-mask-box-image-width: 10px; mask-repeat: no-repeat;`);
+    -webkit-mask-box-image-width: 10px; mask-repeat: no-repeat;`
+    );
     // outer
-    $(e).parent().attr('style',`-webkit-mask-box-image:${$(e).attr("mask-url")};
-    mask-image:${$(e).attr("mask-url")}; mask-repeat: no-repeat;`)
+    $(e)
+      .parent()
+      .attr(
+        "style",
+        `-webkit-mask-box-image:${$(e).attr("mask-url")};
+    mask-image:${$(e).attr("mask-url")}; mask-repeat: no-repeat;`
+      );
+  });
+
+  // center absolute elements
+  $("[center]").each((i, e) => {
+    
+    $(e).css('left', `calc(50% - ${$(e).width() / 2}px)` );
   });
 });
